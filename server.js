1a64cf57f11915d6bc3503d2f1955bb86daf32c2
arunjayaprakash <arun77733@gmail.com>
2017-08-20 22:19:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -16,12 +16,12 @@ var config = {
 
 
 app.get('/test-db', function(req,res){
-    pool.query('SELECT * FROM test', function (err,result){
+    pool.query('SELECT * FROM test', function (err, result){
         if (err) {
             res.status(500).send(err,toString());
                  }
         else    {
-                res.send(JSON.stringify(result));
+                res.send(JSON.stringify(result.rows));
                  }
         
         
