421f507314527fbb721bd8948703c89edc78ed67
arunjayaprakash <arun77733@gmail.com>
2017-08-20 22:16:59 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -24,8 +24,8 @@ app.get('/test-db', function(req,res){
                 res.send(JSON.stringify(result));
                  }
         
-        }
-    })
+        
+    });
 });
 
 var articleOne = {
